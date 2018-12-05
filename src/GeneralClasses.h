ab20912c742a4a439206458815ae879f461f21a0
roeyazran <roeyazran@campus.technion.ac.il>
2018-12-05 15:34:42 +0200

    Series

---
+++ src/GeneralClasses.h
@@ -0,0 +1,18 @@
+//
+// Created by roei_ on 05/12/2018.
+//
+
+#ifndef DEBROINPROJECT_GENERALCLASSES_H
+#define DEBROINPROJECT_GENERALCLASSES_H
+
+
+class Series{
+public:
+    vector<bool> seire;
+    bool marking_flag;
+    int length;
+    int power2;
+    Series(int length, int power2):seire(vector<bool>(length, false)),marking_flag(false), power2{power2}{};
+    vector<Series> CalcUpperComplexity();
+};
+#endif //DEBROINPROJECT_GENERALCLASSES_H
