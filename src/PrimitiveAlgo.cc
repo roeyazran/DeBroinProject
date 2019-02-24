68e509753807ab7d2c228c4492bc6aa24f98dac5
roeyazran <roeyazran@campus.technion.ac.il>
2019-02-24 17:11:45 +0200

    Main Calc function

--- src/PrimitiveAlgo.cc
+++ src/PrimitiveAlgo.cc
@@ -7,19 +7,6 @@
 #include "GeneralClasses.h"
 using  namespace std;
 
-class SearchResult {
-    int n; //serie parameter
-    vector<ComplexityRes> ComplexitiesVector;
-};
-
-class ComplexityRes{
-    int Complexity;
-    float OverallCounter;
-    float DebroienCounter;
-    float ToviCounter;
-    vector<bool*> DebroienSequenceVector;
-    vector<bool*> TuviSequenceVector;
-};
 
 
 
@@ -27,8 +14,32 @@ void UpgradeSequencesComplexity(vector<Series> series){
 }
 
 SearchResult ClacPrimitive(int n){
-    //Creating basic seires - complexity 0
-    vector<bool> testVec(n, false);
+    SearchResult result(n);
 
-    return SearchResult();
+
+    //Creating basic seires - complexity 0
+    vector<bool> zeroVec(n, false);
+    Series BaseSeries(Get2Power(n),n,zeroVec);
+
+    //Creating Series Vector (contains all possible vectors of the same complexity)
+    vector<Series> SeriesVector;
+    SeriesVector.push_back(BaseSeries);
+    int Complexity = 0;
+    while (SeriesVector.size() != 0){
+
+        //iterating through all series of this complexity
+        vector<Series>::iterator it = SeriesVector.begin();
+        while (it!= SeriesVector.end()){
+            result.ComplexitiesVector[Complexity].OverallCounter++;
+            if((*it).CheckNSubSeiresUniqeness(n+1)){
+                result.ComplexitiesVector[Complexity].TuviCounter++;
+                if((*it).CheckNSubSeiresUniqeness(n)){
+                    result.ComplexitiesVector[Complexity].DebroienCounter++;                }
+            }
+            it++;
+        }
+        Complexity++;
+        UpgradeSequencesComplexity(SeriesVector);
+    }
+    return result;
 }
--- src/PrimitiveAlgo.h
+++ src/PrimitiveAlgo.h
@@ -9,8 +9,31 @@
 #include <vector>
 #include "GeneralClasses.h"
 
-class ComplexityRes;
-class SearchResult;
 
+class ComplexityRes{
+public:
+    int Complexity;
+    float OverallCounter;
+    float DebroienCounter;
+    float TuviCounter;
+//    vector<bool*> DebroienSequenceVector;
+//    vector<bool*> TuviSequenceVector;
+};
+
+class SearchResult {
+public:
+    int n; //series parameter
+    vector<ComplexityRes> ComplexitiesVector;
+    SearchResult(int n):n(n),ComplexitiesVector(vector<ComplexityRes>(n)){};
+};
+
+ostream& operator<<(ostream& os, SearchResult& result){
+    os << "Search Result fon n parameter = " << result.n<<std::endl;
+    for (int i = 0; i < result.ComplexitiesVector.size(); ++i) {
+        os << "Complexity = " << i << "Number of Series: " << result.ComplexitiesVector[i].OverallCounter <<
+           " Number of De-Broien Series:" << result.ComplexitiesVector[i].DebroienCounter << " Number of semi De-Broien Series:" <<
+           result.ComplexitiesVector[i].TuviCounter << std::endl << std::endl;
+    }
+}
 
 #endif //PROJECT_PRIMITIVEALGO_H
